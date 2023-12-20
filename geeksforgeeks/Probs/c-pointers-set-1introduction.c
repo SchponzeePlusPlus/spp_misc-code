e87d80fce222de394de2e303fd21f23906cd6f70
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2023-12-20 22:08:58 +1100

    Create c-pointers-set-1introduction.c

---
+++ geeksforgeeks/Probs/c-pointers-set-1introduction.c
@@ -0,0 +1,18 @@
+//User function Template for C
+
+// https://practice.geeksforgeeks.org/problems/c-pointers-set-1introduction/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+/*
+Given a pointer to a variable a, the task is to complete the function updateVar() which will increment the value of the variable by 10. The function does not returns anything.
+*/
+
+/* This function is invoked by passing the address of the variable.
+    You need to increment the value stored in the variable by 10 */
+
+/* complete the function below */
+
+void updateVar(int *a)
+{
+      // write your code here
+      *a += 10;
+}
